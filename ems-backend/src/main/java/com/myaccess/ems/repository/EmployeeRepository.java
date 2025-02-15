7c6961f93a5a88e6fe0a18d914c3c626ccbc02d8
Durgaprasad Akula <141166554+AKULA-V-S-S-V-D-DURGAPRASAD@users.noreply.github.com>
2025-02-15 14:52:55 +0530

    Add files via upload
    Backend

---
+++ ems-backend/src/main/java/com/myaccess/ems/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.myaccess.ems.repository;
+
+import com.myaccess.ems.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+}
